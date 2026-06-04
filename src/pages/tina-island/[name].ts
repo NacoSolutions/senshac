0ca4b2a5a1500452496bdfc6bfb92347ffa8801d
Roger Navelsaker <roger.navelsaker@gmail.com>
2026-06-04 12:30:13 +0200

    fix: bypass AstroContainer for TinaCMS visual editing endpoint

--- src/pages/tina-island/[name].ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from 'astro';
-import { experimental_createIslandRoute } from '@tinacms/astro/experimental';
-import { getTinaRuntimeEnv } from '../../lib/tina-data';
-import { createIslands } from '../../lib/tina-islands';
-
-export const prerender = false;
-export const ALL: APIRoute = (context) => {
-  return experimental_createIslandRoute(createIslands(getTinaRuntimeEnv(context.locals)))(context);
-};
